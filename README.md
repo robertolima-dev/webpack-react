278ada47841555713be86651bf475ea18e1dc20e
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2021-03-16 10:14:26 -0300

    ajustes projeto

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # WEBPACK & REACT
 
-### Projeto Webpack com React
+## Projeto Webpack com React
+
+### 🛠 Tecnologias
+
+- [x] Webpack
+- [x] Webpack-cli
+- [x] Babel
+- [x] React Router Dom
 
 <!--ts-->
-   <!-- * [Sobre](#Sobre)
-   * [Tabela de Conteudo](#tabela-de-conteudo)
-   * [Instalação](#instalacao) -->
-   * [Como iniciar](#como-usar)
+   * [Como iniciar o projeto](#como-usar)
+      * [Clone - git clone https://github.com/robertolima-dev/webpack-react.git](#clone)
+      * [Dependencias - npm install](#dev)
+<!--te-->
+
+<!--ts-->
+   * [Como iniciar a aplicação](#como-usar)
       * [Local - npm start](#local)
       * [Dev - npm run dev](#dev)
       * [Prod - npm run prod](#prod)
       * [Build - npm run build](#build)
-   <!-- * [Tests](#testes)
-   * [Tecnologias](#tecnologias) -->
 <!--te-->
---
+++ webpack.prod.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
